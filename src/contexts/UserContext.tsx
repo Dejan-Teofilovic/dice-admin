8d72ee9b2fed7c2c45227243d86be6265dd75ae7
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-08-07 00:45:58 +0900

    create readme

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useEffect, useReducer } from 'react';
-import { useNavigate } from 'react-router-dom';
 import api from '../utils/api';
 import {
   ERROR,
--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useMemo } from 'react';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { LOCALSTORAGE_TOKEN_NAME } from '../utils/constants';
 import { getItemOfLocalStorage } from '../utils/functions';
 
